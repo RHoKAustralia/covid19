3020b50ebbfff579ed62d76931f3cc63f0e0b47a
Nik Cubrilovic <nc9@protonmail.com>
2020-03-21 09:37:42 +0000

    Allow selecting database based on DJANGO_DATABASE environment variable

--- covid19/covid19/settings.py
+++ covid19/covid19/settings.py
@@ -77,15 +77,27 @@ WSGI_APPLICATION = 'covid19.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES_AVAILABLE = {
+    'production': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'read_default_file': '/app/sql.cfn',
             },
+    },
+    'development': {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, ".db.sqlite3"),
     }
 }
 
+DB_USING = os.environ.get("DJANGO_DATABASE", "production")
+
+if DB_USING not in DATABASES_AVAILABLE.keys():
+    raise Exception("{} is not one of the available databases. Select one of: {}".format(DB_USING, ", ".join(DATABSES_AVAILABLE.keys())))
+
+DATABASES = {
+    'default': DATABASES_AVAILABLE[DB_USING]
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
